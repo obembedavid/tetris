5dc7bd94eeda365cf5be96597e80c3fcf815b673
obembedavid <139708241+obembedavid@users.noreply.github.com>
2024-07-21 19:29:18 +0100

    game grid

--- code/main.py
+++ code/main.py
@@ -1,4 +1,10 @@
 from settings import *
+from sys import exit
+
+#Components
+from game import Game
+from score import Score
+from preview import Preview
 
 class Main:
     def __init__(self):
@@ -7,7 +13,11 @@ class Main:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         pygame.display.set_caption('Tetris')
         self.clock = pygame.time.Clock()
-        
+
+        #compnenets 
+        self.game = Game()
+        self.score = Score()
+        self.preview = Preview()
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -16,6 +26,12 @@ class Main:
                     exit()
             #display
             self.display_surface.fill(GRAY)
+
+            self.game.run()
+            self.score.run()
+            self.preview.run()
+
+            #updating the game
             pygame.display.update()
             self.clock.tick(60)
 
